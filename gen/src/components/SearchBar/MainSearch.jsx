d8b6059664cdef776330f8dddaf4e6eec0708d8d
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-06-27 12:01:46 +0530

    search bar result styled

--- gen/src/components/SearchBar/MainSearch.jsx
+++ gen/src/components/SearchBar/MainSearch.jsx
@@ -1,10 +1,18 @@
 
 import { useState } from "react";
-
+import './MainSearch.css';
+import { Link } from "react-router-dom";
 function MainSearch() {
 
     const [ query , setQuery] = useState('');
     const [ searchResult , setSearchResult ] = useState({ });
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleFirstClick = () => {
+        if (!isExpanded) {
+        setIsExpanded(true);
+        }
+    };
 
 
 
@@ -50,50 +58,57 @@ function MainSearch() {
                     aria-labelledby="staticBackdropLabel"
                     aria-hidden="true"
                 >
-                    <div className="modal-dialog">
-                    <div className="modal-content">
+                <div className="modal-dialog">
+                    <div className="modal-content ">
                         <div className="modal-header d-flex flex-row align-items-center justify-content-between">
+                            <div className="input-group w-100">
+                                <input
+                                type="text"
+                                className="form-control soft-blur"
+                                placeholder="Search product"
+                                aria-label="Search"
+                                onChange={handleSearch}
+                                onClick={handleFirstClick}
 
-                        <div className="input-group w-100">
-                            <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Search product"
-                            aria-label="Search"
-                            onChange={handleSearch}
-                            />
-                        </div>
-
-                        {/* modal close btn */}
-                        <button
-                            type="button"
-                            className="btn btn-outline-dark rounded btn-sm ms-2"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        >
-                            <span>
-                                <i className="bi bi-x"></i>
-                            </span>
-                        </button>
-                    
-                        </div>
-                        <div className="modal-body py-1">
-                        {/* Search results will appear here once integrated */}
-                        
-                            <div className="mt-4">
-                                {Object.entries(searchResult).map(([category, items]) => (
-                                    <div key={category} className="mb-3">
-                                    <h5>{category}</h5>
-                                    <ul className="list-group">
-                                        {items.map(product => (
-                                        <li key={product.id} className="list-group-item">
-                                            {product.productName}
-                                        </li>
-                                        ))}
-                                    </ul>
-                                    </div>
-                                ))}
+                                />
                             </div>
+                            {/* modal close btn */}
+                            <button
+                                type="button"
+                                className="btn rounded btn-sm ms-2 btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            >
+                            </button>
+                        </div>
+                    <div className= {`modal-body py-1 ${isExpanded ? "expanded" : ""}`}>
+                        {/* Search results will appear here once integrated */}
+                            <div className="result-container mt-2">
+                                <div className="px-1">
+                                    {Object.entries(searchResult).map(([category, items]) => (
+                                        <div key={category} className="category-card mb-3 very-soft-shadow soft-blur">
+                                            <div className="category-header">
+                                                <div className="px-2 py-1 soft-blur">
+                                                    <h5 className="category-title ">{category}</h5>
+                                                </div>
+                                            </div>
+                                            <div className="product-list">
+                                                {items.map((product) => (
+                                                    <div key={product.id} className="product-card soft-blur">
+                                                        <Link to={`/product/${product.id}`} className="text-decoration-none"><div className="product-content">
+                                                            <p className="product-name" data-bs-dismiss="modal" aria-label="Close">{product.productName}</p>
+                                                        </div>
+                                                        </Link>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    ))}
+                                    </div>
+
+
+                    </div>
+
 
 
 
